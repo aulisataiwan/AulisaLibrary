34881e2d71e394705775076d11bb904222752a0e
Andy Li <andy.li@aulisa.com>
2023-12-13 11:06:56 +0800

    xcframework version 3.0_dev_v5
    update xcframework

---
+++ Sources/AulisaLibrary/AUOximeter.xcframework/ios-arm64_x86_64-simulator/AUOximeter.framework/PrivateHeaders/bridge.hpp
@@ -0,0 +1,23 @@
+//
+//  bridge.hpp
+//  AUOximeter
+//
+//  Created by Li Yun Jung on 2023/3/21.
+//  Copyright © 2023 Aulisa. All rights reserved.
+//
+
+#ifndef bridge_hpp
+#define bridge_hpp
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void CPPAnalysis(int beginning_point, float* waveform, int* beat_location, int length, char* beat_type_array, int interpretation);
+int test(int hi);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* bridge_hpp */
