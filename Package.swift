c6ff7710803ac81bfe8c6e35ae36048941acefea
Andy Li <andy.li@aulisa.com>
2024-01-15 16:35:43 +0800

    測試Bundle

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AulisaLibrary",
-            targets: ["AUOximeter","AulisaBundle"]),
+            targets: ["AUOximeter"]),
     ],
     dependencies: [
         //.package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1")
@@ -23,7 +23,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "AUOximeter", path:"./Sources/AulisaLibrary/AUOximeter.xcframework"),
-        .target(name: "AulisaBundle",resources: [.copy("./Sources/AulisaLibrary/AUOximeter.xcframework")])
-     
+
     ]
 )
